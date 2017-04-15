2cb517906e6d2a7af72f8a69f167abdc91671637
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2017-04-15 14:31:13 +0200

    Create empty AppSettings file

---
+++ web/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace web
+{
+    public class AppSettings
+    {
+        
+    }
+}
--- web/Startup.cs
+++ web/Startup.cs
@@ -27,6 +27,11 @@ namespace web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var appSettings = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettings);
+
+
+         
             // Add framework services.
             services.AddMvc();
         }
