5610b33e17844bb2727afdf516a51cd344bbe6da
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2017-04-16 13:30:01 +0200

    Create UserProperties migration

--- web/Models/ApplicationUser.cs
+++ web/Models/ApplicationUser.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 
 namespace web.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
+        public DateTime BirthDate { get; set; }
+        
+        public string FullName
+        {
+            get 
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
     }
 }
