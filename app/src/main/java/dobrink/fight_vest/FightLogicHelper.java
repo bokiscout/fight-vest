190c843599769c4968ebc18077b4e1ef454c6120
Borche Jankovski <borchejankovski@gmail.com>
2017-06-17 18:22:01 +0200

    - increse debug logs - fix deletion of old fights when new one start

--- app/src/main/java/dobrink/fight_vest/FightLogicHelper.java
+++ app/src/main/java/dobrink/fight_vest/FightLogicHelper.java
@@ -1,6 +1,7 @@
 package dobrink.fight_vest;
 
 import android.app.Application;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -35,6 +36,8 @@ public class FightLogicHelper extends Application
     private Date endRoundTime; //used in nextRound, used to get starttime of last round;
 
     public FightLogicHelper(){
+        Log.d("FIGHT LOGIC HELPER", "constructor()" );
+
         fights = new ArrayList<Fight>();
         rounds = new ArrayList<RoundDTO>();
         hits = new ArrayList<Hit>();
@@ -49,6 +52,7 @@ public class FightLogicHelper extends Application
     }
 
     public synchronized static FightLogicHelper getInstance() {
+        Log.d("FIGHT LOGIC HELPER", "getInstance()" );
         if (ALFSInstance == null) {
             ALFSInstance = new FightLogicHelper();
         }
@@ -57,6 +61,7 @@ public class FightLogicHelper extends Application
 
     //Make n fake fights , for testing
     public synchronized void makeFakeFights(int n){
+        Log.d("FIGHT LOGIC HELPER", "makeFakeFights()" );
         if (ALFSInstance == null) {
             ALFSInstance = new FightLogicHelper();
         }
@@ -67,6 +72,7 @@ public class FightLogicHelper extends Application
 
     //Checks if a fight was selected in the FightList
     public synchronized boolean checkIfFightPicked(){
+        Log.d("FIGHT LOGIC HELPER", "checkIfFightPicked()" );
         if (selectedFight == null){
             return false;
         }
@@ -74,6 +80,8 @@ public class FightLogicHelper extends Application
     }
 
     public synchronized void  registerHit() {
+        Log.d("FIGHT LOGIC HELPER", "registerHit()" );
+
         int hitID = hits.size(); //set hitID to be position of Hits list, will be unique, incremented
         Date hitTimestamp = new Date(); // Time of hit
         Fighter hitFighter = selectedFight.getFightFighters().get(player).getFighter(); // Fighter hit
@@ -84,9 +92,13 @@ public class FightLogicHelper extends Application
             addPoints(player,strength);
         }
         hits.add(hit); // add hit event to the list of hits
+
+        Log.d("FIGHT LOGIC HELPER", "registerHit() -> hit: " + hit.toString() );
     }
     //used for fake hits
     public synchronized void registerHitFake() {
+        Log.d("FIGHT LOGIC HELPER", "registerHitFake()" );
+
         Random generator = new Random();
         player = generator.nextInt(2);
         strength = generator.nextInt(2)+1;
@@ -110,6 +122,7 @@ public class FightLogicHelper extends Application
     }
 
     private void addPoints(int player, int strength) {
+        Log.d("FIGHT LOGIC HELPER", "addPoints()" );
         //player: 0 FigterOne, 1+ FighterTwo, is hit, give points to other player
         int points = 0;
         if (strength==1){
@@ -125,12 +138,16 @@ public class FightLogicHelper extends Application
     }
 
     public synchronized void startFight() {
+        Log.d("FIGHT LOGIC HELPER", "startFight()" );
+
         startRoundTime = new Date(); //used to get StartTime of first round
         rounds = new ArrayList<RoundDTO>();
         hits = new ArrayList<Hit>();
         registerPoints = true;
     }
     public synchronized void nextRound() {
+        Log.d("FIGHT LOGIC HELPER", "nextRound()" );
+
         int roundID = rounds.size(); // roundID to be position of Rounds list, will be unique, incremented
         endRoundTime = new Date(); // endTime of last round;
         RoundDTO currentRound = new RoundDTO(roundID, startRoundTime, endRoundTime,MatchID,hits);
@@ -139,6 +156,8 @@ public class FightLogicHelper extends Application
         rounds.add(currentRound); // add round to the list of rounds
     }
     public void endFight() {
+        Log.d("FIGHT LOGIC HELPER", "endFight()" );
+
         registerPoints = false;
         int roundID = rounds.size(); // roundID to be position of Rounds list, will be unique, incremented
         endRoundTime = new Date(); //end time for last round
@@ -150,55 +169,75 @@ public class FightLogicHelper extends Application
     //Getters and Setters
     //
     public void setSelectedFight(Fight selectedFight) {
-        fights = new ArrayList<Fight>();
-        this.selectedFight = selectedFight;
+        Log.d("FIGHT LOGIC HELPER", "setSelectedFight()" );
+
+//        fights = new ArrayList<Fight>();
+//        fights.add(selectedFight);
+//        this.selectedFight = selectedFight;
+
+            FighterOnePoints = 0 ;
+            FighterTwoPoints = 0 ;
+
+            this.selectedFight = selectedFight;
     }
 
     public ArrayList<Fight> getFights() {
+        Log.d("FIGHT LOGIC HELPER", "getFight()" );
         return fights;
     }
 
     public Fight getSelectedFight() {
+        Log.d("FIGHT LOGIC HELPER", "getSelectedFight()" );
         return selectedFight;
     }
 
     public int getMatchID() {
+        Log.d("FIGHT LOGIC HELPER", "getMatchID()" );
         return MatchID;
     }
 
     public void setMatchID(int matchID) {
+        Log.d("FIGHT LOGIC HELPER", "setMatchID()" );
         MatchID = matchID;
     }
 
     public static String getParsedMsg() {
+        Log.d("FIGHT LOGIC HELPER", "getParsedMsg()" );
         return parsedMsg;
     }
 
     public static void setParsedMsg(String parsedMsg) {
+        Log.d("FIGHT LOGIC HELPER", "setParsedMsg" );
         FightLogicHelper.parsedMsg = parsedMsg;
     }
 
     public static int getPlayer() {
+        Log.d("FIGHT LOGIC HELPER", "getPlayer()" );
         return player;
     }
 
     public static void setPlayer(int player) {
+        Log.d("FIGHT LOGIC HELPER", "setPlayer()" );
         FightLogicHelper.player = player;
     }
 
     public static int getStrength() {
+        Log.d("FIGHT LOGIC HELPER", "getStrength()" );
         return strength;
     }
 
     public static void setStrength(int strength) {
+        Log.d("FIGHT LOGIC HELPER", "setStrength()" );
         FightLogicHelper.strength = strength;
     }
 
     public static int getFighterOnePoints() {
+        Log.d("FIGHT LOGIC HELPER", "getFighterOneInfoPoints()" );
         return FighterOnePoints;
     }
 
     public static int getFighterTwoPoints() {
+        Log.d("FIGHT LOGIC HELPER", "getFighterTwoInfoPoints()" );
         return FighterTwoPoints;
     }
 }
